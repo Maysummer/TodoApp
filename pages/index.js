cdfe3df02888e41f7431588fdf0c3fc7e36a5ea3
Maysummer <udojyk@gmail.com>
2024-09-09 22:00:09 +0100

    implement todo functionalities

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,206 @@
-import { useState } from "react";
-import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
+import { Josefin_Sans } from "next/font/google";
+import Image from "next/image";
 
-const inter = Inter({ subsets: ["latin"] });
+const josefinSans = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+});
 
 export default function Home() {
   const [inputValue, setInputValue] = useState("");
-  const [todoItems, setTodoItem] = useState([
-    {
-      name: "Iron clothes",
-      completed: true,
-    },
-  ]);
+  const [todoItems, setTodoItems] = useState([]);
+  const [filter, setFilter] = useState("all");
+
+  const filteredTodos = todoItems.filter((todo) => {
+    if (filter === "completed") return todo.completed;
+    else if (filter === "active") return !todo.completed;
+    return true;
+  });
 
   const handleAdd = (e) => {
-
-    if (inputValue) {
-      const newItem = {
-        name: inputValue,
-        completed: false,
-      };
-
-      setTodoItem([...todoItems, newItem]);
-      setInputValue("")
-    } else {
-      alert("Item name is empty.");
+    if (e.key === "Enter") {
+      e.preventDefault();
+      let taskId = 0;
+      todoItems.forEach((task) => {
+        taskId = Math.max(taskId, task.id + 1);
+        return taskId;
+      });
+      if (inputValue) {
+        const newItem = {
+          id: taskId,
+          name: inputValue,
+          completed: false,
+        };
+        setTodoItems([...todoItems, newItem]);
+        setInputValue("");
+      } else {
+        alert("Item name is empty.");
+      }
     }
   };
 
+  const handleToggle = (id) => {
+    setTodoItems((prevTodoItems) =>
+      prevTodoItems.map((item) =>
+        item.id === id ? { ...item, completed: !item.completed } : item
+      )
+    );
+  };
+
+  const handleDelete = (id) => {
+    setTodoItems((prevItems) => prevItems.filter((todo) => todo.id !== id));
+  };
+
+  const incomplete = () => {
+    return todoItems.filter((todo) => !todo.completed).length;
+  };
+
+  const clearCompletedTodos = () => {
+    setTodoItems((prevItems) => prevItems.filter((todo) => !todo.completed));
+  };
+
+  const handleCurrView = () => {
+    if (filter === "all") {
+    }
+  };
+
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (savedTodos && savedTodos.length > 0) {
+      setTodoItems(savedTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todoItems));
+  }, [todoItems]);
+
   return (
     <main
-      className={` min-h-screen flex-col items-center p-24 ${inter.className}`}
+      className={`bg-[url(/assets/images/bg-mobile-light.jpg)] bg-no-repeat bg-auto min-h-screen flex-col items-center ${josefinSans.className} bg-veryLightGrayishBlue px-6`}
     >
-      <h1 className=" text-2xl mb-4 text-center">Basic to-do app</h1>
+      <div className="flex justify-between items-center py-10">
+        <h1 className="text-2xl font-bold tracking-[10px] text- ml-1 text-veryLightGray">
+          TODO
+        </h1>
+        <Image
+          src="/assets/icons/moon.svg"
+          alt="moon icon"
+          width={20}
+          height={20}
+        ></Image>
+      </div>
+
       <form>
-        <div className=" flex flex-col">
-          <label>Item name</label>
+        <div className="relative">
+          <div className="w-5 h-5 absolute bottom-1/2 left-5 rounded-full border"></div>
           <input
             value={inputValue}
+            placeholder="Create a new todo..."
             onChange={(e) => {
               setInputValue(e.target.value);
             }}
-            className=" border-blue-700 border-[1px] h-12 rounded-xl px-2 mb-2"
+            onKeyDown={(e) => handleAdd(e)}
+            className="h-12 indent-11 rounded-md focus:outline-none p-1.5 mb-4 w-full placeholder:text-sm"
           />
-        </div>
-        <div>
-          <button
-            className=" p-4 px-12 bg-blue-700 rounded-xl text-white"
-            type="button"
-            onClick={handleAdd}
-          >
-            Add
-          </button>
         </div>
       </form>
 
-      <section className="mt-8">
-        <h1 className=" text-2xl mb-4">Items</h1>
-        <ul className="p-0 list-disc">
-          {todoItems.map((item, index) => (
-            <li key={index}>{item.name}</li>
+      <section className="text-sm">
+        <ul>
+          {filteredTodos.map((item, index) => (
+            <div
+              key={index}
+              className="flex items-center bg-white first:rounded-t-md last:rounded-b-md h-12 p-1.5 justify-between mb-0.5"
+            >
+              <div className="ml-3.5">
+                <input
+                  type="checkbox"
+                  name={item.name}
+                  id=""
+                  key={index}
+                  className="hidden"
+                />
+                <label
+                  htmlFor={item.name}
+                  className={`flex items-center gap-2.5 ${
+                    item.completed
+                      ? "line-through text-darkGrayishBlue"
+                      : "no-underline"
+                  }`}
+                >
+                  <span
+                    className={`flex w-5 h-5 rounded-full border justify-center items-center ${
+                      item.completed
+                        ? "bg-gradient-to-br from-gradBlue to-gradPurple"
+                        : ""
+                    }`}
+                    onClick={() => handleToggle(item.id)}
+                  >
+                    <Image
+                      src="/assets/icons/check.svg"
+                      alt="check mark"
+                      width={11}
+                      height={9}
+                    ></Image>
+                  </span>
+                  <span>{item.name}</span>
+                </label>
+              </div>
+
+              <Image
+                alt="delete icon"
+                src="/assets/icons/cross.svg"
+                width={13}
+                height={13}
+                onClick={() => handleDelete(item.id)}
+                className="mr-3.5"
+              />
+            </div>
           ))}
+          <div className="bg-white first:rounded-t-md last:rounded-b-md h-12 text-lightGrayishBlue flex justify-between items-center pl-5 pr-5">
+            <p>
+              {incomplete()} item{incomplete() === 1 ? "" : "s"} left
+            </p>
+            <p onClick={clearCompletedTodos} className="cursor-pointer">
+              Clear Completed
+            </p>
+          </div>
         </ul>
+        <div className="bg-white rounded-md pl-20 pr-20 h-12 flex justify-between items-center text-darkGrayishBlue mt-4">
+          <b
+            className={`cursor-pointer ${
+              filter === "all"
+                ? "bg-gradient-to-br from-gradBlue to-gradPurple bg-clip-text text-transparent"
+                : ""
+            }`}
+            onClick={() => setFilter("all")}
+          >
+            All
+          </b>
+          <b
+            className={`cursor-pointer ${
+              filter === "active"
+                ? "bg-gradient-to-br from-gradBlue to-gradPurple bg-clip-text text-transparent"
+                : ""
+            }`}
+            onClick={() => setFilter("active")}
+          >
+            Active
+          </b>
+          <b
+            className={`cursor-pointer ${
+              filter === "completed"
+                ? "bg-gradient-to-br from-gradBlue to-gradPurple bg-clip-text text-transparent"
+                : ""
+            }`}
+            onClick={() => setFilter("completed")}
+          >
+            Completed
+          </b>
+        </div>
       </section>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,28 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        // Primary
+        brightBlue: "hsl(220, 98%, 61%)",
+        gradBlue: 'hsl(192, 100%, 67%)',
+        gradPurple: 'hsl(280, 87%, 65%)',
+
+        // Light Theme
+        veryLightGray: "hsl(0, 0%, 98%)",
+        veryLightGrayishBlue: "hsl(236, 33%, 92%)",
+        lightGrayishBlue: "hsl(233, 11%, 84%)",
+        darkGrayishBlue: "hsl(236, 9%, 61%)",
+        veryDarkGrayishBlue: "hsl(235, 19%, 35%)",
+
+        // Dark Theme
+        veryDarkBlue: "hsl(235, 21%, 11%)",
+        veryDarkDesaturatedBlue: "hsl(235, 24%, 19%)",
+        lightGrayishBlue: "hsl(234, 39%, 85%)",
+        hoverLightGrayishBlue: "hsl(236, 33%, 92%)",
+        darkGrayishBlue: "hsl(234, 11%, 52%)",
+        veryDarkGrayishBlue: "hsl(233, 14%, 35%)",
+        veryDarkGrayishBlue: "hsl(237, 14%, 26%)",
+      },
     },
   },
   plugins: [],
